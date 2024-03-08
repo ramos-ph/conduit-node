2fd640b2f5910c5234946c1704dab578345403e3
Pedro Ramos <pedroramos.contato@outlook.com.br>
2024-03-08 17:03:31 -0300

    Add base userController

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 const fastify = require("fastify");
+const userController = require("./controllers/user-controller");
 
 const makeApp = (options = {}) => {
   const app = fastify(options);
 
   app.get("/", async (request, reply) => {
     return { message: "Hello, World" };
+  });
+
+  app.route({
+    method: "POST",
+    url: "/api/users",
+    handler: userController.create,
   });
 
   return app;
---
+++ src/controllers/user-controller.js
@@ -0,0 +1,24 @@
+const { Profile } = require("../models/profile");
+
+module.exports = {
+  async create(request, reply) {
+    const { username, email, password } = request.body.user;
+
+    // temporarily encoding it as base64
+    const passwordHash = Buffer.from(password).toString("base64");
+
+    const user = await Profile.create({ username, email, passwordHash });
+
+    reply.status(201);
+
+    return {
+      user: {
+        username: user.username,
+        email: user.email,
+        bio: user.bio,
+        image: user.image,
+        token: null,
+      },
+    };
+  },
+};
