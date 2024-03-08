cdfa32b46363697f41039c691f04e8f93d42a520
Pedro Ramos <pedroramos.contato@outlook.com.br>
2024-03-08 20:43:00 -0300

    Remove sequelize.sync()

--- src/server.js
+++ src/server.js
@@ -5,7 +5,6 @@ const server = require("./app")({ logger: true });
 const start = async () => {
   try {
     await sequelize.authenticate();
-    await sequelize.sync({ force: true });
     await server.listen({ port: 3000 });
   } catch (err) {
     server.log.error(err);
